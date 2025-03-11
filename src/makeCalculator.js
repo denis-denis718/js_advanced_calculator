6510f6be81c4560e453fd18313d1c4d5cbff0f3c
Denis <denchick1546450@gmail.com>
2025-03-11 20:58:46 +0200

    add calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(num) {
+      return this.result + num;
+    },
+
+    subtract(num) {
+      return this.result - num;
+    },
+
+    multiply(num) {
+      return this.result * num;
+    },
+
+    divide(num) {
+      return this.result / num;
+    },
+
+    operate(fn, num) {
+      this.result = fn.call(this, num); // Применяем функцию с контекстом
+
+      return this; // Чейнинг
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this; // Чейнинг
+    },
+  };
 }
 
 module.exports = makeCalculator;
